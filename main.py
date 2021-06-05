39267cac2a0be75a181e849dbdc233174b4d4ce3
jeanpaulHT <jean.huby@utec.edu.pe>
2021-06-05 15:51:13 -0500

    sup

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
-from nltk.stem import SnowballStemmer
+from preprocesamiento import preproccesing
 
-stemmer = SnowballStemmer('spanish')
+def creatingFrequency():
+    words = {}
+    for book in range(1,7):
+        f = open("texts/preprocessing/libro" + str(book) + ".txt")
+        for word in f:
+            if word != " " and word != "\n":
+                word = word[:-1]
+                if word in words:
+                    words[word][0] = words[word][0] + 1
+                    if book not in words[word][1]:
+                        words[word][1].append(book)
 
+                else:
+                    words[word] = [1, [book]]
 
-def loadUneccesaryElemts():
-    s = {".", "?", "!", "¿", "<", ">", ",", "º", " ", ":", ";", "«", "»"}
+    print("a")
 
-    return s
-
-
-def loadStopList():
-    f = open("texts/stoplist.txt", encoding="ISO-8859-1")
-    stopList = set()
-    for line in f:
-        # when reading it gives a \n char, we ignored it
-        stopList.add(line[:-1]) 
-    return stopList
-
-
-def parse(str):
-    wordList = str.split(" ")
-    res = list()
-    unusedElem = loadUneccesaryElemts()
-    for word in wordList:
-        for c in word:
-            if (c in unusedElem):
-                word = word.replace(c, "")
-        res.append(word)
-
-    return res
-
-
-def read_documents(book):
-    file = "texts/books/libro" + str(book) + ".txt"
-    f = open(file, encoding="utf-8")
-    out = open("texts/preprocessing/libro" + str(book) + ".txt", "w+", encoding="utf-8")
-    stoplist = loadStopList()
-    for line in f:
-        if line != "\n" and line != " ":
-            for word in parse(line):
-                if word.lower() not in stoplist:
-                    out.write(stemmer.stem(word.lower()) + "\n")
-    f.close()
-    out.close()
 
 
 if __name__ == "__main__":
-    for i in range(1, 7):
-        print("done " + str(i))
-        read_documents(i)
+    # preprocesamiento.preproccesing()
+    print("running main....")
+    creatingFrequency()
+
+
+
+
+
