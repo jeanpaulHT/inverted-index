e783591013b75c6669aeb728dee52c1db5b988fb
jeanpaulHT <jean.huby@utec.edu.pe>
2021-06-05 14:41:24 -0500

    some refactoring done

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-import nltk
 from nltk.stem import SnowballStemmer
-
 
 stemmer = SnowballStemmer('spanish')
 
-def loadUneccesaryChar():
-    file = "texts/stoplist.txt"
-    f = open(file)
+
+def loadUneccesaryElemts():
     s = {".", "?", "!", "¿", "<", ">", ",", "º", " ", ":", ";", "«", "»"}
 
-
     return s
+
 
 def loadStopList():
     f = open("texts/stoplist.txt", encoding="ISO-8859-1")
     stopList = set()
     for line in f:
-        stopList.add(line[:-1])
+        # when reading it gives a \n char, we ignored it
+        stopList.add(line[:-1]) 
     return stopList
+
 
 def parse(str):
     wordList = str.split(" ")
     res = list()
-    uneWords =loadUneccesaryChar()
+    unusedElem = loadUneccesaryElemts()
     for word in wordList:
         for c in word:
-            if(c in uneWords):
+            if (c in unusedElem):
                 word = word.replace(c, "")
         res.append(word)
 
     return res
+
+
 def read_documents(book):
     file = "texts/books/libro" + str(book) + ".txt"
     f = open(file, encoding="utf-8")
     out = open("texts/preprocessing/libro" + str(book) + ".txt", "w+", encoding="utf-8")
     stoplist = loadStopList()
     for line in f:
-        if line != "\n":
+        if line != "\n" and line != " ":
             for word in parse(line):
                 if word.lower() not in stoplist:
-                    out.write(stemmer.stem(word.lower()) + " ")
+                    out.write(stemmer.stem(word.lower()) + "\n")
     f.close()
     out.close()
 
 
 if __name__ == "__main__":
-    # read_documents(1)
-    read_documents(1)
-    print("a");
+    for i in range(1, 7):
+        print("done " + str(i))
+        read_documents(i)
